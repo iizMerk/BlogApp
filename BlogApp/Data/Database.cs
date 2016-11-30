9b0b76cb903d45943d60c2ead3251618ded2be55
iizMerk <merkmoi2@gmail.com>
2016-11-30 19:36:13 +0100

    Created The Database For: -Users -Posts -Comments Added the connection string.

---
+++ BlogApp/Data/Database.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace BlogApp.ViewModels
+{
+    public class Database:DbContext
+    {
+        DbSet<User> Users { get; set; }
+        DbSet<Post> Posts { get; set; }
+
+        DbSet<Comment> Comments { get; set; }
+
+        public Database() : base("name=Connection") { }
+
+    }
+}
---
+++ BlogApp/Data/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BlogApp.ViewModels
+{
+    public class User
+    {
+        [Key]
+        public int UserID { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+        public string Email { get; set; }
+        public virtual ICollection<Post> Post { get; set; } = new List<Post>();
+    }
+}
